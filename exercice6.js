6f1e07ac2f4a39d5863605a8b2f630029854a5fc
AndreVictor60 <andre.victor60@free.fr>
2017-10-24 12:18:06 +0200

    Vérification de la saisie de l'utilisateur

--- exercice6.js
+++ exercice6.js
@@ -2,6 +2,12 @@ function resultat() {
   // Déclarer les variables & récuperer les deux nombres saisie par utilisateur
   var number1 = document.getElementById('premier_nombre').value;
   var number2 = document.getElementById('deuxieme_nombre').value;
-  // Afficher le reste de la division
-  alert('Le reste de la division de '+number1+ ' % ' +number2+ ' est ' +number1%number2);
+  // vérificaation
+    if(isNaN(number1) == true || isNaN(number2) == true){
+      alert('Ceci n\'est pas un texte');
+    }
+    else {
+        // Afficher le reste de la division
+      alert('Le reste de la division de '+number1+ ' % ' +number2+ ' est ' +number1%number2);
+    }
 }
